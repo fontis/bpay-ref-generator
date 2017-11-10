9c3f3015ce5a54243542f6f73a6a0235341eee23
Sam Greenwood <sam@samgreenwood.me>
2017-11-10 16:14:18 +1030

    Move generator instantiation to setUp method

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -1,15 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 use Fontis\BpayRefGenerator\Generator;
 
 class GeneratorTest extends TestCase
 {
+    private $generator;
+
+    public function setUp() {
+        $this->generator = new Generator();
+    }
+
     public function testItGeneratesValidMod10V1()
     {
-        $generator = new Generator();
-
-        $number = $generator->calcMod10V1('1234567890');
+        $number = $this->generator->calcMod10V1('1234567890');
 
         $this->assertSame('12345678903', $number);
 
@@ -17,9 +23,7 @@ class GeneratorTest extends TestCase
 
     public function testItGeneratesValidMod10V5()
     {
-        $generator = new Generator();
-
-        $number = $generator->calcMod10V5('1234567890');
+        $number = $this->generator->calcMod10V5('1234567890');
 
         $this->assertSame('12345678905', $number);
     }
